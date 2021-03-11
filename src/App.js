e13c02376845da17346cde447356956347e400ca
aripan <aripan017@gmail.com>
2021-03-11 18:35:44 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import TeamDetails from "./components/TeamDetails/TeamDetails";
